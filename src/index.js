1b96b7fbbe780432c002419deabd98db9fa9ac53
John Doe <naduha.9612@gmail.com>
2023-01-19 21:08:50 +0300

    feat: finish the task

--- src/index.js
+++ src/index.js
@@ -37,10 +37,82 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
-}
-
+// function decode(expr) {
+//     // write your solution here
+// }
+// function morseDecoder (string) {
+//     const morse = {
+//     a: ".-",
+//     b: "-...",
+//     c: "-.-.",
+//     d: "-..",
+//     e: ".",
+//     f: "..-.",
+//     g: "--.",
+//     h: "....",
+//     i: "..",
+//     j: ".---",
+//     k: "-.-",
+//     l: ".-..",
+//     m: "--",
+//     n: "-.",
+//     o: "---",
+//     p: ".--.",
+//     q: "--.-",
+//     r: ".-.",
+//     s: "...",
+//     t: "-",
+//     u: "..-",
+//     v: "...-",
+//     w: ".--",
+//     x: "-..-",
+//     y: "-.--",
+//     z: "--..",
+//     1: ".----",
+//     2: "..---",
+//     3: "...--",
+//     4: "....-",
+//     5: ".....",
+//     6: "-....",
+//     7: "--...",
+//     8: "---..",
+//     9: "----.",
+//     0: "-----"
+//   };
+function decode(string) {
+  let arr = []
+   for (let i = 0; i<string.length; i+=9) {
+     arr.push(string.substring(i, i+10));
+     i++;
+   }
+    let arr2 = [];
+    for (let i=0; i<arr.length; i++) {
+      let newEl = "";
+      if (arr[i] === "**********") {
+        newEl = newEl+arr[i].replaceAll('**', '');
+        arr2.push(newEl);
+      }
+      else {
+      newEl = newEl + arr[i].replaceAll('00', '').replaceAll('10', '.').replaceAll('11', '-');
+      arr2.push(newEl);
+      }
+    }
+  let result = "";
+    for (let i of arr2) {
+      if (i === "") {
+        result = result + " ";
+      }
+      else {
+      for (let o in MORSE_TABLE) {
+     if (i === o) {
+       result = result + MORSE_TABLE[o];
+     }
+      }
+      }
+    }
+    return result;
+  }
+  
 module.exports = {
     decode
 }
